ded51254d575078e00a36e87bcb7643d00d18171
aeugster <40519239+aeugster@users.noreply.github.com>
2022-11-17 21:26:11 +0100

    Whitelist of deserializable classes (#22)

--- src/main/java/tools/nexus/secure_tcp_socket/common/ObjInputStream.java
+++ src/main/java/tools/nexus/secure_tcp_socket/common/ObjInputStream.java
@@ -1,15 +1,46 @@
 package tools.nexus.secure_tcp_socket.common;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ObjInputStream {
 
-    ObjectInputStream input;
+    private final Set<String> allowedClasses;
+
+    private final ObjectInputStream input;
 
     public ObjInputStream(InputStream inputStream) throws IOException {
-        input = new ObjectInputStream(inputStream);
+
+        allowedClasses = new HashSet<>();
+        allowedClasses.add("tools.nexus.secure_tcp_socket.dto.Message");
+
+        // ExampleClientIT
+        allowedClasses.add("java.security.KeyRep");
+        allowedClasses.add("[B");
+        allowedClasses.add("java.security.KeyRep$Type");
+        allowedClasses.add("java.lang.Enum");
+        allowedClasses.add("java.util.HashMap");
+
+        // file transfer
+        allowedClasses.add("java.lang.Long");
+        allowedClasses.add("java.lang.Number");
+
+        input = new ObjectInputStream(inputStream) {
+
+            /**
+             * Deserialization should not be vulnerable to injection attacks
+             */
+            @Override
+            protected Class<?> resolveClass(ObjectStreamClass osc) throws IOException, ClassNotFoundException {
+                // Only deserialize instances of AllowedClass
+                if (!allowedClasses.contains(osc.getName())) {
+                    throw new InvalidClassException("Unauthorized deserialization", osc.getName());
+                }
+                return super.resolveClass(osc);
+            }
+
+        };
     }
 
     public Object readUnshared() throws ClassNotFoundException, IOException {
